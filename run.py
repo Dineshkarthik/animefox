7affb3199e5233efcd50cf8f08238e7a468c1678
Dineshkarthik <dineshkarthik.r@gmail.com>
2021-10-02 16:26:54 +0200

    fix: uWsgi deployment

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from app import app
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
